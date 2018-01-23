ada4a114e8b29a1564a1f28108eaadb6263766a4
Johnny Miller <johnnysviva@outlook.com>
2018-01-23 23:32:08 +0800

    Update CustomerOrder.java

--- src/entity/CustomerOrderEntity.java
+++ src/entity/CustomerOrderEntity.java
@@ -7,8 +7,17 @@ public class CustomerOrderEntity {
     private int userId;
     private Timestamp orderDatetime;
     private int movieScheduleId;
+    private String movieTitle;
+    private Timestamp showtime;
+    private String seatId;
+    private String seatLocation;
+    private String auditoriumName;
+    private String theaterName;
+    private String theaterLocation;
     private boolean isPaid;
+    private Timestamp paymentDatetime;
     private boolean isUsed;
+    private Timestamp usedDatetime;
     private int ticketAmount;
     private float totalPrice;
 
@@ -44,12 +53,76 @@ public class CustomerOrderEntity {
         this.movieScheduleId = movieScheduleId;
     }
 
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public Timestamp getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(Timestamp showtime) {
+        this.showtime = showtime;
+    }
+
+    public String getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(String seatId) {
+        this.seatId = seatId;
+    }
+
+    public String getSeatLocation() {
+        return seatLocation;
+    }
+
+    public void setSeatLocation(String seatLocation) {
+        this.seatLocation = seatLocation;
+    }
+
+    public String getAuditoriumName() {
+        return auditoriumName;
+    }
+
+    public void setAuditoriumName(String auditoriumName) {
+        this.auditoriumName = auditoriumName;
+    }
+
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    public void setTheaterName(String theaterName) {
+        this.theaterName = theaterName;
+    }
+
+    public String getTheaterLocation() {
+        return theaterLocation;
+    }
+
+    public void setTheaterLocation(String theaterLocation) {
+        this.theaterLocation = theaterLocation;
+    }
+
     public boolean getIsPaid() {
         return isPaid;
     }
 
-    public void setIsPaid(boolean paid) {
-        isPaid = paid;
+    public void setIsPaid(boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public Timestamp getPaymentDatetime() {
+        return paymentDatetime;
+    }
+
+    public void setPaymentDatetime(Timestamp paymentDatetime) {
+        this.paymentDatetime = paymentDatetime;
     }
 
     public boolean getIsUsed() {
@@ -58,6 +131,14 @@ public class CustomerOrderEntity {
 
     public void setIsUsed(boolean isUsed) {
         this.isUsed = isUsed;
+    }
+
+    public Timestamp getUsedDatetime() {
+        return usedDatetime;
+    }
+
+    public void setUsedDatetime(Timestamp usedDatetime) {
+        this.usedDatetime = usedDatetime;
     }
 
     public int getTicketAmount() {
