ea985e1f567f01116fe0d92f98be1af397316e8c
Johnny Miller <johnnysviva@outlook.com>
2018-01-24 16:38:36 +0800

    Remove codes validating the parameters

--- src/servlet/administrator/UserManagement.java
+++ src/servlet/administrator/UserManagement.java
@@ -17,6 +17,7 @@ public class UserManagement extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
+
         String radio = request.getParameter("userOperation");
         System.out.println("userOperation: " + radio);
         switch (radio) {
@@ -48,6 +49,27 @@ public class UserManagement extends HttpServlet {
                     e.printStackTrace();
                 }
                 break;
+            case "getAll":
+                try {
+                    getAll(request, response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "getOnline":
+                try {
+                    getOnlineUser(request, response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "getJson":
+                try {
+                    getJson(request, response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
             default:
         }
     }
@@ -64,26 +86,27 @@ public class UserManagement extends HttpServlet {
         String email = request.getParameter("email");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        if (email.compareTo("") != 0 && username.compareTo("") != 0 && password.compareTo("") != 0) {
-            // Set Entity and save
-            user.setEmail(email);
-            user.setUsername(username);
-            user.setPassword(password);
-            userDao.save(user);
 
+        // Task of checking the validations of parameter is assigned to web browser
+
+        // Set Entity and save
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        int status = userDao.save(user);
+
+        if (status > 0) {
             // Go to administrator.jsp
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement: added.');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " add: Success.');window.location.href='/administrator.jsp'</script>");
             out.flush();
             out.close();
         } else {
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement: Parameter error.');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " add: Failure.');window.history.go(-1)</script>");
             out.flush();
             out.close();
         }
-
-
     }
 
     private void update(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -95,17 +118,26 @@ public class UserManagement extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        // Task of checking the validations of parameter is assigned to web browser
+
         // Set Entity and save
         user.setEmail(email);
         user.setUsername(username);
         user.setPassword(password);
-        userDao.update(user);
+        int status = userDao.update(user);
 
-        // Go to administrator.jsp
-        PrintWriter out = response.getWriter();
-        out.println("<script>alert('UserManagement updated.');window.location.href='/administrator.jsp'</script>");
-        out.flush();
-        out.close();
+        if (status > 0) {
+            // Go to administrator.jsp
+            PrintWriter out = response.getWriter();
+            out.println("<script>alert('" + getClass() + " update: Success.');window.location.href='/administrator.jsp'</script>");
+            out.flush();
+            out.close();
+        } else {
+            PrintWriter out = response.getWriter();
+            out.println("<script>alert('" + getClass() + " update: Failure.');window.history.go(-1)</script>");
+            out.flush();
+            out.close();
+        }
     }
 
     private void delete(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -115,19 +147,21 @@ public class UserManagement extends HttpServlet {
         // Get Parameter
         String email = request.getParameter("email");
 
+        // Task of checking the validations of parameter is assigned to web browser
+
         // Delete by email
         user.setEmail(email);
         int status = userDao.delete(user);
         if (status == 1) {
             // Go to administrator.jsp
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement deleted.');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " delete: Success.');window.location.href='/administrator.jsp'</script>");
             out.flush();
             out.close();
         } else {
             // Go to administrator.jsp
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement deleted: failed.');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " delete: Failure.');window.history.go(-1)</script>");
             out.flush();
             out.close();
         }
@@ -140,22 +174,30 @@ public class UserManagement extends HttpServlet {
         // Get Parameter
         String email = request.getParameter("email");
 
-        // Delete by email
+        // Query by email
         user.setEmail(email);
         user = userDao.queryByEmail(user);
 
         if (user != null) {
             // Go to administrator.jsp
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement query." + user.getEmail() + ":" + user.getPassword() +
-                    "');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " query: User found.');window.location.href='/administrator.jsp'</script>");
             out.flush();
             out.close();
         } else {
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('UserManagement query: NOT FOUND.');window.location.href='/administrator.jsp'</script>");
+            out.println("<script>alert('" + getClass() + " query: User not found.');;window.history.go(-1)</script>");
             out.flush();
             out.close();
         }
+    }
+
+    private void getAll(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void getOnlineUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void getJson(HttpServletRequest request, HttpServletResponse response) {
     }
 }
