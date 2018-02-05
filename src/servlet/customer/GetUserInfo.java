7bf1ccd58c8a3d6d48c1333781facc104891d4dc
Johnny Miller <johnnysviva@outlook.com>
2018-02-05 21:28:57 +0800

    ✅Finish GetUserInfo.java

--- src/servlet/customer/GetUserInfo.java
+++ src/servlet/customer/GetUserInfo.java
@@ -1,19 +1,50 @@
 package servlet.customer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dao.UserDao;
+import entity.UserEntity;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "GetUserInfo")
 public class GetUserInfo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/json");
 
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDao dao = new UserDao();
+        UserEntity entity = new UserEntity();
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+
+        entity.setEmail(email);
+        entity.setPassword(password);
+        entity = dao.queryByEmailAndPassword(entity);
+        if (entity != null) {
+            PrintWriter out = response.getWriter();
+            String json = gson.toJson(entity);
+            out.write(json);
+            out.flush();
+            out.close();
+        } else {
+            PrintWriter out = response.getWriter();
+            out.write("null");
+            out.flush();
+            out.close();
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
